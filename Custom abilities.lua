f3e396ad811d2324ab1b8818fcb22a6f42e83d1a
NaturalNi13 <naturalidiot1234@gmail.com>
2025-01-07 06:33:39 -0600

    Update Custom abilities.lua

--- Custom abilities.lua
+++ Custom abilities.lua
@@ -1,1 +1,7 @@
+--Info
+loadstring(game:HttpGet('https://raw.githubusercontent.com/NaturalNi13/Custom-Abilities/refs/heads/main/notify.lua'))()
 
+--Abilities
+loadstring(game:HttpGet('https://raw.githubusercontent.com/NaturalNi13/Custom-Abilities/refs/heads/main/cglide.lua'))()
+
+--Fuck you john cena you ain't stealing my abilities anymore
