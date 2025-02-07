d7ab13e5ff7e52d80bb5e6a51c4e8b6e54f2e675
NaturalNi13 <naturalidiot1234@gmail.com>
2025-02-07 04:27:57 -0600

    Update customabilitie.lua

--- customabilitie.lua
+++ customabilitie.lua
@@ -2,8 +2,9 @@
 loadstring(game:HttpGet('https://raw.githubusercontent.com/NaturalNi13/Custom-Abilities/refs/heads/main/notify.lua'))()
 
 --Abilities
-loadstring(game:HttpGet('https://raw.githubusercontent.com/NaturalNi13/Custom-Abilities/refs/heads/main/cglide.lua'))()
-loadstring(game:HttpGet('https://raw.githubusercontent.com/NaturalNi13/Custom-Abilities/refs/heads/main/slevitate.lua'))()
-loadstring(game:HttpGet('https://raw.githubusercontent.com/NaturalNi13/Custom-Abilities/refs/heads/main/giaction.lua'))()
-
+if game.PrivateServerId ~= "" then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/NaturalNi13/Custom-Abilities/refs/heads/main/cglide.lua'))()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/NaturalNi13/Custom-Abilities/refs/heads/main/slevitate.lua'))()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/NaturalNi13/Custom-Abilities/refs/heads/main/giaction.lua'))()
+end
 --Fuck you john cena you ain't stealing my abilities anymore
